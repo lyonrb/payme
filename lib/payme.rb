adf5accb025034c8b743d4567f38a09c773770f7
Damien Mathieu <damien.mathieu@lim.eu>
2010-07-07 09:30:06 +0200

    there's not always a Gemfile in the project

--- lib/payme.rb
+++ lib/payme.rb
@@ -1,6 +1,11 @@
-require "rubygems"
-require "bundler"
-Bundler.setup
+begin
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+rescue  Bundler::GemfileNotFound
+  # There is no gemfile for this project (rails 2.3 ?)
+end
+  
 
 require 'payme/config'
 require 'payme/errors'
