6d78d25512d2c095fb20e7afc335f87a7554f599
Damien Mathieu <damien.mathieu@lim.eu>
2010-07-07 12:06:54 +0200

    oh come on

--- lib/payme.rb
+++ lib/payme.rb
@@ -1,13 +1,4 @@
-begin
-  require "rubygems"
-  require "bundler"
-  Bundler.setup
-rescue
-  # There is no gemfile for this project (rails 2.3 ?)
-end
-  
-
 require 'payme/config'
 require 'payme/errors'
 require 'payme/request'
-require 'payme/response'
+require 'payme/response'e
